e37da10b82aea522245f5a628fc0c8a265bba96f
Adrian Ałaszewski <wns2148810@interia.pl>
2020-07-21 15:06:49 +0200

    Temporary controller

---
+++ src/main/java/pki/sqlTables/Controller.java
@@ -0,0 +1,17 @@
+package pki.sqlTables;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping
+    public String index(@RequestParam(required = false) String name) {
+        if(name == null || name.equals("")) {
+            name = "World";
+        }
+        return "Hello, " + name + "!";
+    }
+}
