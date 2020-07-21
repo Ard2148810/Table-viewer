584952770b19d881d4b90845fcdbde24b22527c4
Adrian Ałaszewski <wns2148810@interia.pl>
2020-07-21 14:48:52 +0200

    Init

---
+++ src/main/java/pki/sqlTables/SqlTablesApplication.java
@@ -0,0 +1,13 @@
+package pki.sqlTables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlTablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlTablesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pki/sqlTables/SqlTablesApplicationTests.java
@@ -0,0 +1,13 @@
+package pki.sqlTables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlTablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
