11244adb003dccae2584361cf010bf4f411f10c7
Adrian Ałaszewski <wns2148810@interia.pl>
2020-08-12 15:54:42 +0200

    Added example React view

---
+++ src/main/resources/static/precache-manifest.f1a42c1b86969a2505afb341d0aa0644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7a9e58d5b991bf40b54a275034de21",
+    "url": "./index.html"
+  },
+  {
+    "revision": "773b1c82634d92182eea",
+    "url": "./static/css/main.5644ad2f.chunk.css"
+  },
+  {
+    "revision": "0a4f4bc4df516e4e566b",
+    "url": "./static/js/2.610dc31e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.610dc31e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "773b1c82634d92182eea",
+    "url": "./static/js/main.0b7c7ea3.chunk.js"
+  },
+  {
+    "revision": "3a8678e6b1cc4b32c6e5",
+    "url": "./static/js/runtime-main.d78c5e8a.js"
+  }
+]);
